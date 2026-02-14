1dfd678164978bc920e2ed59cea883f73435eed4
phdiamantino <51421386+phdiamantino@users.noreply.github.com>
2026-02-14 13:30:11 -0300

    fix: add server.py

--- server.py
+++ server.py
@@ -110,5 +110,11 @@ async def health(_):
     return json({"status": "ok"})
 
 
+#if __name__ == "__main__":
+#    app.run(host="0.0.0.0", port=1337, workers=4)
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=1337, workers=4)
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 8000))
+    )
