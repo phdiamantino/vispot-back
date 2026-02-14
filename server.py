a0afb35b590db284f291c2ed14e0bc83b04316dc
phdiamantino <51421386+phdiamantino@users.noreply.github.com>
2026-02-14 15:50:35 -0300

    fix: open cors for all origins

--- server.py
+++ server.py
@@ -17,19 +17,15 @@ app = Sanic("tcc_api")
 #app.config.CORS_ORIGINS = "*"
 #Extend(app)
 
-allowed_origins = [
-    "https://vispot-front.vercel.app",
-    "http://localhost:3000", 
-    "http://localhost:5173"
-]
 
 CORS(app, resources={
     r"/*": {
-        "origins": allowed_origins,
+        "origins": "*",
         "methods": ["GET", "POST", "OPTIONS"],
         "allow_headers": ["Content-Type", "Authorization"]
     }
 })
+
 
 MOCK_FOLDER = "mocks"
 MOCK_FILES = {
